d5da1a2e3a0f2fd919e1f2a051155088dc05f162
Kelsey Warshawsky <kjgasser@gmail.com>
2023-09-29 14:24:22 -0500

    first commit

--- lib/aen-cdk-stack.ts
+++ lib/aen-cdk-stack.ts
@@ -1,16 +1,111 @@
+
 import * as cdk from 'aws-cdk-lib';
+import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AenCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, 'aen-api', {
+      name: 'aen-api',
+      schema: appsync.SchemaFile.fromAsset('schema/schema.graphql'),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AenCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //creates a DDB table
+    const add_ddb_table = new dynamodb.Table(this, 'characters-table', {
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+    });
+
+    const add_func = new appsync.AppsyncFunction(this, 'func-get-character', {
+      name: 'get_characters_func_1',
+      api,
+      dataSource: api.addDynamoDbDataSource('table-for-characters', add_ddb_table),
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+          return { operation: 'Scan' };
+          }
+
+          export function response(ctx) {
+          return ctx.result.items;
+          }
+  `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+    });
+
+    const add_func_2 = new appsync.AppsyncFunction(this, 'func-add-character', {
+      name: 'add_characters_func_1',
+      api,
+      dataSource: api.addDynamoDbDataSource('table-for-characters-2', add_ddb_table),
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+            return {
+            operation: 'PutItem',
+            key: util.dynamodb.toMapValues({id: util.autoId()}),
+            attributeValues: util.dynamodb.toMapValues(ctx.args.input),
+            };
+          }
+
+          export function response(ctx) {
+            return ctx.result;
+          }
+      `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+    });
+
+    // Adds a pipeline resolver with the get function
+    new appsync.Resolver(this, 'pipeline-resolver-get-characters', {
+      api,
+      typeName: 'Query',
+      fieldName: 'getCharacters',
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+          return {};
+          }
+
+          export function response(ctx) {
+          return ctx.prev.result;
+          }
+  `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [add_func],
+    });
+
+    // Adds a pipeline resolver with the create function
+    new appsync.Resolver(this, 'pipeline-resolver-create-characters', {
+      api,
+      typeName: 'Mutation',
+      fieldName: 'createCharacter',
+      code: appsync.Code.fromInline(`
+          export function request(ctx) {
+          return {};
+          }
+
+          export function response(ctx) {
+          return ctx.prev.result;
+          }
+  `),
+      runtime: appsync.FunctionRuntime.JS_1_0_0,
+      pipelineConfig: [add_func_2],
+    });
+
+    // Prints out URL
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl
+    });
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || ''
+    });
+
+    // Prints out the stack region to the terminal
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region
+    });
   }
 }
