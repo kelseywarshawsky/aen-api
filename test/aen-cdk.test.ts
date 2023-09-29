8a643e322b2cf5de410877ede1d0b6e2b8667cc2
Kelsey Warshawsky <kjgasser@gmail.com>
2023-09-29 12:51:58 -0500

    Initial commit

---
+++ test/aen-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AenCdk from '../lib/aen-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aen-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AenCdk.AenCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
